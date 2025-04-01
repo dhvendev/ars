c6d660d097825a5338c38f4c83fc902062ac8161
AlohaFan999 <arsenij00000777@gmail.com>
2025-04-01 20:46:13 +0300

    добавил тесты)

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,6 +8,20 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
+
+
+def test_password_lenght():
+    assert len(generate_password(100)) == 100
+
+
+def test_password_not_the_same():
+    for i in range(1, 101):
+        assert generate_password(i) != generate_password(i)
+
+
+def test_password_unique():
+    assert generate_password(8) not in [generate_password(8) for _ in range(1000000)]
+
 """
 Допиши еще один тест из предложенных. Или придумай свой.
 Если сможешь написать больше, то будет круто!
